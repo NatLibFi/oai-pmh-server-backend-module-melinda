c94b31ce3131d56bfd903f04a7008b2702e607dd
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2017-09-18 10:28:49 +0300

    :art: Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Melinda backend module for oai-pmh-server [![NPM Version](https://img.shields.io/npm/v/@natlibfi/oai-pmh-server-backend-module-melinda.svg)](https://npmjs.org/package/@natlibfi/oai-pmh-server-backend-module-melinda) [![Build Status](https://travis-ci.org/NatLibFi/oai-pmh-server-backend-module-melinda.svg)](https://travis-ci.org/NatLibFi/oai-pmh-server-backend-module-melinda) [![Test Coverage](https://codeclimate.com/github/NatLibFi/oai-pmh-server-backend-module-melinda/badges/coverage.svg)](https://codeclimate.com/github/NatLibFi/oai-pmh-server-backend-module-melinda/coverage)
+
+Melinda backend module for oai-pmh-server
+
+## License and copyright
+
+Copyright (c) 2017 **University Of Helsinki (The National Library Of Finland)**
+
+This project's source code is licensed under the terms of **GNU Affero General Public License Version 3** or any later version.
---
+++ test/index.spec.js
@@ -0,0 +1,36 @@
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file. 
+*
+* Melinda backend module for oai-pmh-server
+*
+* Copyright (C) 2017 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of oai-pmh-server-backend-module-melinda
+*
+* oai-pmh-server-backend-module-melinda program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* oai-pmh-server-backend-module-melinda is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+/* eslint-disable no-undef, max-nested-callbacks, no-unused-expressions */
+
+'use strict';
+
+import {expect} from 'chai';
+import * as testContext from '../source/index';
+
+describe.skip('index');
